05851e1e55268b913928a762de77581f72665f37
Donghyun Lee <shubug@naver.com>
2025-07-15 18:08:33 +0900

    feat: add base 'TextMotion' component

--- src/TextMotion/TextMotion.tsx
+++ src/TextMotion/TextMotion.tsx
@@ -1,3 +1,20 @@
-export const TextMotion = () => {
-  return <h1>React TextMotion</h1>;
+import './TextMotion.scss';
+
+type TextMotionProps = {
+  as?: keyof React.JSX.IntrinsicElements;
+  text: string;
+};
+
+export const TextMotion: React.FC<TextMotionProps> = ({ as = 'span', text }) => {
+  const Tag = as;
+
+  return (
+    <Tag aria-label={text}>
+      {text.split('').map((item, index) => (
+        <span key={index} style={{ animation: `fade 0.25s ease-out ${index * 0.025}s both` }} aria-hidden="true">
+          {item === ' ' ? '\u00A0' : item}
+        </span>
+      ))}
+    </Tag>
+  );
 };

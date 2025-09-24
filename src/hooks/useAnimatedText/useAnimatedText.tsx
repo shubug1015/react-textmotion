986ffa5485b73f6f8c996f287c264eec19f65547
Donghyun Lee <shubug@naver.com>
2025-09-24 22:38:58 +0900

    docs: add JSDocs of 'useAnimatedText' hook

--- src/hooks/useAnimatedText/useAnimatedText.tsx
+++ src/hooks/useAnimatedText/useAnimatedText.tsx
@@ -12,6 +12,19 @@ type UseAnimatedProps = {
   onAnimationEnd?: () => void;
 };
 
+/**
+ * @description
+ * `useAnimatedText` is a custom hook that animates an array of strings.
+ * It manages the animation sequence index to apply delays correctly.
+ *
+ * @param {string[]} splittedText - The array of strings to be animated.
+ * @param {number} initialDelay - The initial delay before the animation starts, in seconds.
+ * @param {AnimationOrder} animationOrder - Defines the order in which the animation sequence is applied. Defaults to `'first-to-last'`.
+ * @param {Motion} resolvedMotion - The motion configuration object, which is a result of merging custom motion and presets.
+ * @param {() => void} onAnimationEnd - Callback function that is called when the animation ends.
+ *
+ * @returns {ReactNode[]} An array of animated React nodes.
+ */
 export const useAnimatedText = ({
   splittedText,
   initialDelay,
